6d131fad9e68f55f8405b0d15062918239c5700d
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-06-01 21:33:18 +0800

    feat(Sinuka): add id prop

--- Sinuka/Core/Models/User.py
+++ Sinuka/Core/Models/User.py
@@ -1,9 +1,11 @@
 class User:
+    id = ""
     username = ""
     hashedPassword = ""
     email = ""
 
     def __init__(self, id, username, password, email):
+        self.id = id
         self.username = username
         self.hashedPassword = password
         self.email = email
