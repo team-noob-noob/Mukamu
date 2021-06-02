136b1871efe6c9bc770cf6a406970d53d8ea71fc
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-06-02 19:14:50 +0800

    feat: entity

---
+++ Sinuka/src/Sinuka.Core/Models/IEntity.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Sinuka.Core.Models
+{
+    public interface IEntity
+    {
+        public Guid Id { get; set; }
+
+        DateTime CreatedAt { get; set; }
+        DateTime UpdatedAt { get; set; }
+        DateTime DeletedAt { get; set; }
+    }
+}
--- Sinuka/src/Sinuka.Core/Models/Session.cs
+++ Sinuka/src/Sinuka.Core/Models/Session.cs
@@ -2,12 +2,14 @@ using System;
 
 namespace Sinuka.Core.Models
 {
-    public class User : IEntity
+    public class Session : IEntity
     {
         public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string HashedPassword { get; set; }
-        public string Email { get; set; }
+        public Guid User_Id { get; set; }
+        public string Token { get; set; }
+
+        public virtual User user { get; set; }
+
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public DateTime DeletedAt { get; set; }
