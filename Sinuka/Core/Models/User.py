17845cd19bce66da2212c007e7154b9ee55473ac
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-06-02 09:50:01 +0800

    refactor(Sinuka): follow PEP standard, remove shadowing

--- Sinuka/Core/Models/User.py
+++ Sinuka/Core/Models/User.py
@@ -1,11 +1,11 @@
 class User:
-    id = ""
+    id_ = ""
     username = ""
     hashedPassword = ""
     email = ""
 
-    def __init__(self, id, username, password, email):
-        self.id = id
+    def __init__(self, id_, username, password, email):
+        self.id_ = id_
         self.username = username
         self.hashedPassword = password
         self.email = email
