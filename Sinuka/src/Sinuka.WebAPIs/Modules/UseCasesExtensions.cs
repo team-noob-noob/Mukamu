051240ccc639b0de54ca93751a8562571caaeaab
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-06-04 10:06:22 +0800

    feat(Sinuka): add login use case to DI

--- Sinuka/src/Sinuka.WebAPIs/Modules/UseCasesExtensions.cs
+++ Sinuka/src/Sinuka.WebAPIs/Modules/UseCasesExtensions.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Sinuka.Application.UseCases.Register;
+using Sinuka.Application.UseCases.Login;
 
 namespace Sinuka.WebAPIs.Modules
 {
@@ -8,6 +9,7 @@ namespace Sinuka.WebAPIs.Modules
         public static IServiceCollection AddUseCases(this IServiceCollection services)
         {
             services.AddScoped<IRegisterUseCase, RegisterUseCase>();
+            services.AddScoped<ILoginUseCase, LoginUseCase>();
 
             return services;
         }
