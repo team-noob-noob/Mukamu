eb0ff8e2e3d5f4344fc013db0f859db7e0d3f851
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-06-07 20:44:19 +0800

    chore(Mukamu): initial commit

---
+++ src/Mukamu.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Mukamu.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Mukamu.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Mukamu.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
