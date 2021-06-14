1d3da6efa1a8273c71dfa789b3ff863063cbd1bb
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-06-14 19:10:41 +0800

    fix(Sinuka): missing unit of work saving

--- Sinuka/src/Sinuka.Application/UseCases/LogOut/LogoutUseCase.cs
+++ Sinuka/src/Sinuka.Application/UseCases/LogOut/LogoutUseCase.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Sinuka.Core.Interfaces.Repositories;
+using Sinuka.Application.Interfaces;
 
 namespace Sinuka.Application.UseCases.Logout
 {
@@ -7,9 +8,15 @@ namespace Sinuka.Application.UseCases.Logout
     {
         private ILogoutPresenter _presenter;
         private readonly ISessionRepository _sessionRepo;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public LogoutUseCase(ISessionRepository sessionRepository)
-            => this._sessionRepo = sessionRepository;
+        public LogoutUseCase(
+            ISessionRepository sessionRepository,
+            IUnitOfWork unitOfWork)
+        {
+            this._sessionRepo = sessionRepository;
+            this._unitOfWork = unitOfWork;
+        }
 
         public async Task Run(LogoutInput input)
         {
@@ -18,7 +25,8 @@ namespace Sinuka.Application.UseCases.Logout
             if(session is not null)
             {
                 this._sessionRepo.RemoveSession(session);
-                
+                await this._unitOfWork.Save();
+
                 if(session.Client.PostLogoutRedirect is not null)
                 {
                     this._presenter.Redirect(session.Client.PostLogoutRedirect);
@@ -27,7 +35,6 @@ namespace Sinuka.Application.UseCases.Logout
             }
             
             this._presenter.Done();
-            
         }
 
         public void SetPresenter(ILogoutPresenter presenter)
