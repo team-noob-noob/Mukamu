e5e6b92bae04ed44e65d67d734044751284cef0e
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-06-14 14:57:04 +0800

    fix(Sinuka): add hangfire server

--- Sinuka/src/Sinuka.WebAPIs/Startup.cs
+++ Sinuka/src/Sinuka.WebAPIs/Startup.cs
@@ -52,6 +52,7 @@ namespace Sinuka.WebAPIs
                 };
             var storage = new MySqlStorage(Sinuka.Infrastructure.Configurations.DbConfig.DbConnectionString, options);
             services.AddHangfire(x => x.UseStorage(storage));
+            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
