43391c7ffd34e92f735d345ac09ad9ae2f78eeee
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-07-01 22:28:10 +0800

    fix(Sinuka): incorrect operation

--- Sinuka/src/Sinuka.Infrastructure/Database/ModelFactory.cs
+++ Sinuka/src/Sinuka.Infrastructure/Database/ModelFactory.cs
@@ -30,7 +30,7 @@ namespace Sinuka.Infrastructure.Database
 
         public SessionToken CreateSessionToken(dynamic payload)
         {
-            var expiresAt = DateTime.Now - Sinuka.Infrastructure.Configurations.TokenConfig.TokenLifetimeLength;
+            var expiresAt = DateTime.Now + Sinuka.Infrastructure.Configurations.TokenConfig.TokenLifetimeLength;
             payload.ExpiresAt = expiresAt;
             var token = JwtTokenGenerator.GeneratorToken(payload);
             return new SessionToken(token, expiresAt);
