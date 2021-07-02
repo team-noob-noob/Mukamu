4199eba7700c0d60735e390f7ee3b73d93377ca5
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-07-02 21:32:42 +0800

    fix(Sinuka): add use case to DI

--- Sinuka/src/Sinuka.WebAPIs/Modules/UseCasesExtensions.cs
+++ Sinuka/src/Sinuka.WebAPIs/Modules/UseCasesExtensions.cs
@@ -3,6 +3,7 @@ using Sinuka.Application.UseCases.Register;
 using Sinuka.Application.UseCases.Login;
 using Sinuka.Application.UseCases.Authorization;
 using Sinuka.Application.UseCases.Logout;
+using Sinuka.Application.UseCases.Refresh;
 
 namespace Sinuka.WebAPIs.Modules
 {
@@ -14,6 +15,7 @@ namespace Sinuka.WebAPIs.Modules
             services.AddScoped<ILoginUseCase, LoginUseCase>();
             services.AddScoped<IAuthorizationUseCase, AuthorizationUseCase>();
             services.AddScoped<ILogoutUseCase, LogoutUseCase>();
+            services.AddScoped<IRefreshUseCase, RefreshUseCase>();
 
             return services;
         }
