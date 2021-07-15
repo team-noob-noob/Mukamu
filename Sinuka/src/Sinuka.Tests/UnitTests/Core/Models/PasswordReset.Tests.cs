c22f588e16e8dd89408c0470f731f0207acb505d
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-07-15 18:46:54 +0800

    test(Sinuka): password reset tests

---
+++ Sinuka/src/Sinuka.Tests/UnitTests/Core/Models/PasswordReset.Tests.cs
@@ -0,0 +1,45 @@
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Sinuka.Core.Models;
+
+namespace Sinuka.Tests.UnitTests.Core.Models 
+{
+    [TestFixture, Category("Unit")]
+    public class PasswordResetTests
+    {
+        private PasswordReset _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            this._sut = new PasswordReset(new User("", "", new EmailAddress("", "")), "TEST", DateTime.Now);
+        }
+
+        [Test]
+        public void IsExpiredShouldReturnTrue_IfExpiresAtLessThanDateNow()
+        {
+            // Arrange
+            this._sut.ExpiresAt = DateTime.Now - new TimeSpan(1, 0, 0);
+
+            // Act
+            var result = this._sut.IsExpired();
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsExpiredShouldReturnFalse_IfExpiresAtGreaterThanDateNow()
+        {
+            // Arrange
+            this._sut.ExpiresAt = DateTime.Now + new TimeSpan(1, 0, 0);
+
+            // Act
+            var result = this._sut.IsExpired();
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
