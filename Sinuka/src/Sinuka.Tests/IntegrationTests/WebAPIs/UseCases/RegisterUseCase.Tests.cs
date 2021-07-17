4ac98f95b442b253b68bc01401dd058745085f52
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-07-17 11:43:54 +0800

    tests(Sinuka): register integration tests

---
+++ Sinuka/src/Sinuka.Tests/IntegrationTests/WebAPIs/UseCases/RegisterUseCase.Tests.cs
@@ -0,0 +1,98 @@
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using NUnit.Framework;
+using Bogus;
+using FluentAssertions;
+using Sinuka.WebAPIs;
+using Sinuka.Application.UseCases.Register;
+
+namespace Sinuka.Tests.IntegrationTests.WebAPIs.UseCases
+{
+    [TestFixture, Category("integration")]
+    public class RegisterUseCaseTests
+    {
+        private CustomWebApplicationFactory<Startup> _factory;
+
+        private RegisterInput input = new RegisterInput()
+        {
+            Username = new Bogus.DataSets.Internet().UserName(),
+            Password = new Bogus.DataSets.Internet().Password(),
+            Email = new Bogus.DataSets.Internet().Email(),
+        };
+
+        [SetUp]
+        public void Setup()
+        {
+            this._factory = new CustomWebApplicationFactory<Startup>();
+        }
+
+
+        [Test]
+        public async Task Register_ShouldReturn200_IfGivenUnusedEmailAndUsername()
+        {
+            // Arrange
+            var client = this._factory.CreateClient();
+            var body = JsonContent.Create<RegisterInput>(input);
+
+            // Act
+            var response = await client.PostAsync("/user/register", body);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Test]
+        public async Task Register_ShouldReturn400_IfGivenUsedEmail()
+        {
+            // Arrange
+            var client = this._factory.CreateClient();
+            var badInput = new RegisterInput()
+            {
+                Username = new Bogus.DataSets.Internet().UserName(),
+                Password = new Bogus.DataSets.Internet().Password(),
+                Email = input.Email,
+            };
+            var body = JsonContent.Create<RegisterInput>(badInput);
+
+            // Act
+            var response = await client.PostAsync("/user/register", body);
+
+            // Assert
+            response.StatusCode.Should().Be(400);
+        }
+
+        [Test]
+        public async Task Register_ShouldReturn400_IfGivenUsedUsername()
+        {
+            // Arrange
+            var client = this._factory.CreateClient();
+            var badInput = new RegisterInput()
+            {
+                Username = input.Username,
+                Password = new Bogus.DataSets.Internet().Password(),
+                Email = new Bogus.DataSets.Internet().Email(),
+            };
+            var body = JsonContent.Create<RegisterInput>(badInput);
+
+            // Act
+            var response = await client.PostAsync("/user/register", body);
+
+            // Assert
+            response.StatusCode.Should().Be(400);
+        }
+
+        [Test]
+        public async Task Regiser_ShouldReturn400_IfGivenEmptyInput()
+        {
+            var client = this._factory.CreateClient();
+            var badInput = new RegisterInput();
+            var body = JsonContent.Create<RegisterInput>(badInput);
+
+            // Act
+            var response = await client.PostAsync("/user/register", body);
+
+            // Assert
+            response.StatusCode.Should().Be(400);
+        }
+    }
+}
