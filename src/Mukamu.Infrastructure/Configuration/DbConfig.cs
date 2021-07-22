a3210fbe3d1fbd8d6bb9dac5ede9e20a487febd6
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-07-22 21:17:06 +0800

    chore(Mukamu): rename database

--- src/Mukamu.Infrastructure/Configuration/DbConfig.cs
+++ src/Mukamu.Infrastructure/Configuration/DbConfig.cs
@@ -6,7 +6,7 @@ namespace Mukamu.Infrastructure.Configurations
     {
         public static string DbConnectionString 
             = Environment.GetEnvironmentVariable("SINUKA_DB_CONN_STR")
-            ?? "server=localhost;database=sinukaka;uid=root;pwd=root;";
+            ?? "server=localhost;database=mukamu;uid=root;pwd=root;";
         public static System.Version MySqlVersion = new System.Version(
             Environment.GetEnvironmentVariable("SINUKA_DB_VER_MAJ") is not null && int.TryParse(Environment.GetEnvironmentVariable("SINUKA_DB_VER_MAJ"), out int maj) ? maj : 5, 
             Environment.GetEnvironmentVariable("SINUKA_DB_VER_MIN") is not null && int.TryParse(Environment.GetEnvironmentVariable("SINUKA_DB_VER_MIN"), out int min) ? min : 7, 
